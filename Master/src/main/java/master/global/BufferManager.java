d75819ec83875d83ab1665ef5769947aa74787be
surevil <445073309@qq.com>
2018-05-12 00:26:18 +0800

    save and find complete

--- Master/src/main/java/master/global/BufferManager.java
+++ Master/src/main/java/master/global/BufferManager.java
@@ -4,6 +4,6 @@ import master.global.model.Buffer;
 import master.global.model.L2Buffer;
 
 public class BufferManager {
-    public static Buffer buffer = new Buffer();
-    public static L2Buffer l2Buffer = new L2Buffer();
+    public static volatile Buffer buffer = new Buffer();
+    public static volatile L2Buffer l2Buffer = new L2Buffer();
 }
