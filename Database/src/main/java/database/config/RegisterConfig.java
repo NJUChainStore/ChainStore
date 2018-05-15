05cd3eaa81e3c5aed3583a963e11fde0abaa52a1
tonywang1945yes <wangruihua88@yahoo.com>
2018-05-15 09:48:41 +0800

    存储机端口随机生成

--- Database/src/main/java/database/config/RegisterConfig.java
+++ Database/src/main/java/database/config/RegisterConfig.java
@@ -4,6 +4,7 @@ import database.model.GlobalData;
 import database.model.RegisterParameters;
 import database.model.Role;
 import database.response.RegisterResponse;
+import database.util.PortUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,10 +14,15 @@ import org.springframework.web.client.RestTemplate;
 public class RegisterConfig {
     @Value("${url.server}")
     private static String masterUrl;
+
     public  static void registerToMaster() {
+        String localUrl="http://localhost:";
+        int portNum= PortUtil.getRandomPort();
+        localUrl=localUrl+Integer.toString(portNum);
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        HttpEntity<RegisterParameters> entity = new HttpEntity<>(new RegisterParameters("http://localhost:8081"), headers);
+        HttpEntity<RegisterParameters> entity = new HttpEntity<>(new RegisterParameters(localUrl), headers);
         String url =masterUrl+"/register/" + Role.DATABASE;
         RegisterResponse registerResponse = restTemplate.exchange(url, HttpMethod.POST, entity, RegisterResponse.class).getBody();
         GlobalData.getInstance().setAccessToken(registerResponse.getAccessToken());
