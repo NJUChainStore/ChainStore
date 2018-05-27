19d4a4079bcb2482548f8bc87e698ddb898a38aa
surevil <445073309@qq.com>
2018-05-27 17:35:01 +0800

    edit about shell

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,18 +3,35 @@ sudo chmod +x webservice.sh
 sudo chmod +x mining.sh
 
 if [ "$1" == "run" ]; then
-./master.sh run
-tail -f -n 1 master.txt | echo | grep -qe "Started Master"
+./master.sh run 8000
+tail -f -n 1 master8000.txt | echo | grep -qe "Started Master"
+./master.sh run 8001
+tail -f -n 1 master8001.txt | echo | grep -qe "Started Master"
+./master.sh run 8002
+tail -f -n 1 master8002.txt | echo | grep -qe "Started Master"
 ./mining.sh run
 ./webservice.sh run
 else if [ "$1" == "start" ]; then
-./master.sh start
-tail -f master.txt | while read LOGLINE
+./master.sh start 8000
+tail -f master8000.txt | while read LOGLINE
+do
+[[ "${LOGLINE}" == *"Started Master"* ]] && pkill -P $$ tail
+done
+./master.sh start 8001
+tail -f master8001.txt | while read LOGLINE
+do
+[[ "${LOGLINE}" == *"Started Master"* ]] && pkill -P $$ tail
+done
+./master.sh start 8002
+tail -f master8002.txt | while read LOGLINE
 do
 [[ "${LOGLINE}" == *"Started Master"* ]] && pkill -P $$ tail
 done
 ./mining.sh start
 ./webservice.sh start
+./database.sh start 8082
+./database.sh start 8083
+./database.sh start 8084
 else if [ "$1" == "stop" ]; then
 ./master.sh stop
 ./mining.sh stop
