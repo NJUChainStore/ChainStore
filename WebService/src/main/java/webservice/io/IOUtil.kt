e8e32eaac45a7120165a9b231be2f243b908376a
Chen Junda <smallda@outlook.com>
2018-05-29 10:02:18 +0800

    change the way path is got

--- WebService/src/main/java/webservice/io/IOUtil.kt
+++ WebService/src/main/java/webservice/io/IOUtil.kt
@@ -13,20 +13,9 @@ object IOUtil {
     }
     @JvmStatic
     fun getFilePathUnderRootDirOfJarFileOrClassDir(relativePath: String): String {
-        //        String path = MainApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        val path = WebService::class.java.protectionDomain.codeSource.location.path
-        val paths = path.split("!".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        val filePath = paths[0]
-
-        val jarFile = File(paths[0])
-        val parentDir = Arrays.stream(jarFile.parent.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
-                .skip(1).collect(Collectors.joining(":"))
-
-
-        println(parentDir)
-        return parentDir + relativePath
-
-        //        return path + ".." + relativePath;
+        val path = File(".").absolutePath + relativePath
+        println(path)
+        return path
     }
 
 }
